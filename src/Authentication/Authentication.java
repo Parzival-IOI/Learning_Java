507009fe8a1b82d7e991c1832197af1e0729f8bb
Kimhouy HOR <kimhouy.hor@allweb.com.kh>
2024-03-16 10:22:43 +0700

    hashing password and dashboard

--- src/Authentication/Authentication.java
+++ src/Authentication/Authentication.java
@@ -1,5 +1,9 @@
 package Authentication;
 
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,7 +12,17 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 public class Authentication {
+    private final Map<String, Integer> auths;
+    public Authentication() {
+        auths = new HashMap<>(4);
+        String default_user = "Admin";
+        auths.put(default_user, default_user.hashCode());
+    }
     public void buildAuthentication() {
+        login();
+    }
+
+    private void login() {
         JFrame frame = new JFrame();
 
         JLabel user = new JLabel("User Name");
@@ -24,15 +38,42 @@ public class Authentication {
 
         JButton login = new JButton("Login");
         JButton clear = new JButton("Clear");
+        JButton register = new JButton("Register");
 
         user.setBounds(5, 5, 200, 60);
         password.setBounds(5, 65, 200, 60);
 
-        userField.setBounds(205, 5, 180, 60);
-        passwordField.setBounds(205, 65, 180, 60);
+        userField.setBounds(205, 5, 175, 60);
+        passwordField.setBounds(205, 65, 175, 60);
 
         clear.setBounds(5, 125, 190, 60);
-        login.setBounds(205, 125, 180, 60);
+        register.setBounds(5, 190, 190, 60);
+        login.setBounds(205, 125, 175, 60);
+
+        clear.addActionListener(e -> {
+            userField.setText("");
+            passwordField.setText("");
+        });
+        login.addActionListener(e -> {
+            String user_name = userField.getText();
+            String password_credential = new String(passwordField.getPassword());
+            boolean isUser = false;
+
+            for (var entry : auths.entrySet()) {
+                if (Objects.equals(entry.getKey(), user_name) && Objects.equals(entry.getValue(), password_credential.hashCode())) {
+                    isUser = true;
+                    break;
+                }
+            }
+
+            if(isUser) {
+                landingPage();
+                frame.dispose();
+                return;
+            }
+            userField.setText("");
+            passwordField.setText("");
+        });
 
         frame.add(user);
         frame.add(password);
@@ -40,9 +81,27 @@ public class Authentication {
         frame.add(passwordField);
         frame.add(clear);
         frame.add(login);
+        frame.add(register);
 
         frame.setSize(400, 400);
         frame.setLayout(null);
         frame.setVisible(true);
     }
+
+    private void landingPage() {
+        JFrame frame = new JFrame("DashBoard");
+
+        JLabel label = new JLabel("Welcome to DashBoard");
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+
+        label.setBounds(100, 100, 200, 200);
+
+        frame.add(label);
+
+        frame.setSize(400, 400);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
 }
